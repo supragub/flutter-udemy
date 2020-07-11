dd240fcdc4b4eb660b8a654c3bdcb61d56ea6af6
Gabor Varga <gabor.varga@suprasoft.hu>
2020-07-11 21:15:07 +0200

    class Person

--- second_try/lib/main.dart
+++ second_try/lib/main.dart
@@ -1,9 +1,19 @@
+class Person {
+  String name ='Max';
+  int age = 30;
+}
+
 double addNumbers(double num1, double num2) {
   // print (num1 + num2);
   return num1 + num2;
 }
 
 void main() {
+  var p1 = Person();
+  var p2 = Person();
+  p2.name = 'Manu';
+  print(p1.name);
+  print(p2.name);
   double firstResult;
   firstResult = addNumbers(1, 1);
   // ...
