9ccb67c422ec17d06f5601856b9c86cf51d00773
Gabor Varga <gabor.varga@suprasoft.hu>
2020-07-11 23:11:37 +0200

    constructor with required argument

--- second_try/lib/main.dart
+++ second_try/lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
 class Person {
-  String name ='Max';
-  int age = 30;
+  String name;
+  int age;
+
+  Person({@required String inputName, int age = 30}) {
+    name = inputName;
+    this.age = age; 
+  }
 }
 
 double addNumbers(double num1, double num2) {
@@ -9,8 +16,8 @@ double addNumbers(double num1, double num2) {
 }
 
 void main() {
-  var p1 = Person();
-  var p2 = Person();
+  var p1 = Person(inputName: 'Max');
+  var p2 = Person(age: 31, inputName: 'Manu');
   p2.name = 'Manu';
   print(p1.name);
   print(p2.name);
