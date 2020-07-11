5587b83e879befe924a76185be3a4f6483520332
Gabor Varga <gabor.varga@suprasoft.hu>
2020-07-11 19:57:07 +0200

    addition function

---
+++ second_try/lib/main.dart
@@ -0,0 +1,10 @@
+double addNumbers(double num1, double num2) {
+  // print (num1 + num2);
+  return num1 + num2;
+}
+
+void main() {
+  print(addNumbers(1, 2.6));
+  print(addNumbers(1, 1));
+  print('Hello!');
+}
