85f9378ee929917097776733488d5bf866816adc
Gábor Varga <gabor.varga@suprasoft.hu>
2020-07-13 21:27:34 +0200

    answerQuestion

--- third_flutter_complete_guide/app_002_my_first_app/lib/main.dart
+++ third_flutter_complete_guide/app_002_my_first_app/lib/main.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -15,15 +25,15 @@ class MyApp extends StatelessWidget {
             Text('The question!'),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
           ],
         ),
