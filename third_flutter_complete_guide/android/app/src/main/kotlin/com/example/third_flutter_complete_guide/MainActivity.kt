7ace361f2af3a0f8f7423c1ccaf143816e9ccaa0
Gábor Varga <gabor.varga@suprasoft.hu>
2020-07-13 17:44:46 +0200

    Remove duplicated directory

--- third_flutter_complete_guide/android/app/src/main/kotlin/com/example/third_flutter_complete_guide/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.third_flutter_complete_guide
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- third_flutter_complete_guide/app_001_hello/android/app/src/main/kotlin/com/example/app_001_hello/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.app_001_hello
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
