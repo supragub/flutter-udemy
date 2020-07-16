7eb072223ff6128aab7886ff7789eafa709feadc
Gábor Varga <gabor.varga@suprasoft.hu>
2020-07-16 21:11:33 +0200

    Answer selectHandler

--- third_flutter_complete_guide/app_002_my_first_app/lib/main.dart
+++ third_flutter_complete_guide/app_002_my_first_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './question.dart';
+import './answer.dart';
 
 void main() => runApp(MyApp());
 
@@ -35,21 +36,12 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Question(questions[_questionIndex]),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: _answerQuestion,
+            Question(
+              questions[_questionIndex],
             ),
-            RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: () => print('Answer 2 chosen!'),
-            ),
-            RaisedButton(
-              child: Text('Answer 3'),
-              onPressed: () {
-                print('Answer 3 chosen');
-              },
-            ),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
           ],
         ),
       ),
