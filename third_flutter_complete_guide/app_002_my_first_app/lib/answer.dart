08088c97765f6f30990378a2f600a5ac36c49b7f
Gábor Varga <gabor.varga@suprasoft.hu>
2020-07-16 22:10:49 +0200

    mapping Lists of questions

--- third_flutter_complete_guide/app_002_my_first_app/lib/answer.dart
+++ third_flutter_complete_guide/app_002_my_first_app/lib/answer.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 class Answer extends StatelessWidget {
 
   final Function selectHandler;
+  final String answerText;
 
-  Answer(this.selectHandler);
+  Answer(this.selectHandler, this.answerText);
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +13,8 @@ class Answer extends StatelessWidget {
       width: double.infinity,
       child: RaisedButton(
         color: Colors.blue,
-        child: Text('Answer 1'),
+        textColor: Colors.white,
+        child: Text(answerText),
         onPressed: selectHandler,
       ),
     );
