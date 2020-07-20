5cf7ea93a07254b267162c116242f2872438acb8
Gábor Varga <gabor.varga@suprasoft.hu>
2020-07-20 15:57:11 +0200

    Restart Quiz

--- third_flutter_complete_guide/app_002_my_first_app/lib/result.dart
+++ third_flutter_complete_guide/app_002_my_first_app/lib/result.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+  final Function resetHandler;
 
-  Result(this.resultScore);
+  Result(this.resultScore, this.resetHandler);
 
   String get resultPhrase {
     var resultText;
@@ -22,9 +23,18 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+          ),
+          FlatButton(
+            onPressed: resetHandler,
+            child: Text('Restart Quiz!'),
+            textColor: Colors.blue,
+          ),
+        ],
       ),
     );
   }
